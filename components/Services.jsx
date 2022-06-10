6aa8c94241402793a4399beaf0d8fc7a6bfbeae6
Telickers <hello@telickers.net>
2022-06-10 17:01:44 +0300

    Update & improve services section

--- components/Services.jsx
+++ components/Services.jsx
@@ -4,58 +4,78 @@ import { AiFillAccountBook } from "../node_modules/react-icons/ai";
 export default function Services() {
   let services = [
     {
-      src: "/tag.svg",
+      src: "/icons/code.svg",
       title: "Web Development",
-      desc: ["Generating new websites", "Fixing bugs", "Improving performance"],
+      desc: [
+        "Advise businesses regarding software use",
+        "Set up online software tools test test test",
+        "Assess the technological needs of a company",
+      ],
     },
     {
-      src: "/paint.svg",
-      title: "Design",
-      desc: ["Generating new websites", "Fixing bugs", "Improving performance"],
+      src: "/icons/palet.svg",
+      title: "UI / UX Design",
+      desc: [
+        "Advise businesses regarding software use",
+        "Set up online software tools test test test",
+        "Assess the technological needs of a company",
+      ],
     },
     {
-      src: "/brain.svg",
-      title: "Teaching",
-      desc: ["Generating new websites", "Fixing bugs", "Improving performance"],
+      src: "/icons/brain.svg",
+      title: "Tutoring",
+      desc: [
+        "Advise businesses regarding software use",
+        "Set up online software tools test test test",
+        "Assess the technological needs of a company",
+      ],
     },
     {
-      src: "/lamp.svg",
-      title: "Consultation",
-      desc: ["Generating new websites", "Fixing bugs", "Improving performance"],
+      src: "/icons/lamp.svg",
+      title: "Software Consultation",
+      desc: [
+        "Advise businesses regarding software use",
+        "Set up online software tools test test test",
+        "Assess the technological needs of a company",
+      ],
     },
   ];
 
   return (
-    <>
-      <div className="">
-        <header className="my-10 flex justify-center text-7xl">Services</header>
-        <div className="-mb-2 grid w-screen grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          {services.map((serv, idx) => (
-            <article key={idx}>
-              <img
-                src={serv.src}
-                className="z-10 mx-auto h-32 rounded-full bg-white"
-                alt=""
-              />
-              <div className="text text-center">
-                <h1 className="mb-5 mt-2 text-3xl">{serv.title}</h1>
+    <section className="bg-red-300 py-4">
+      <div className="flex justify-center flex-col items-center">
+        <h2 className="pb-16 text-center text-5xl font-bold">Our Services</h2>
+        <div className="grid w-screen grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10">
+          {services.map((serv) => (
+            <article key={serv.src}>
+              <div className="flex items-center justify-center bg-black w-20 h-20 rounded-full mx-auto">
+                <img
+                  src={serv.src}
+                  className="mx-auto w-10 h-10"
+                  alt={serv.title}
+                />
+              </div>
+              <div className="text text-center pt-3">
+                <span className="block pb-1 text-2xl font-bold">
+                  {serv.title}
+                </span>
                 {serv.desc.map((d, i) => (
-                  <p key={i}>{d}</p>
+                  <p className="text-sm" key={i}>{d}</p>
                 ))}
               </div>
             </article>
           ))}
         </div>
-        <div className="z-0 mx-44 h-0.5 -translate-y-48 bg-black"></div>
+        {/* <div className="z-0 mx-44 h-0.5 -translate-y-48 bg-black"></div> */}
       </div>
 
-      <div className="my-20 flex justify-center">
-        <Link href="/">
-          <a className=" rounded-3xl bg-black px-8 py-3 text-white">
+      <div className="mt-20 flex justify-center">
+        <Link href="/services">
+          <a className="rounded-3xl bg-black px-8 py-3 text-white">
             Read More
           </a>
         </Link>
       </div>
-    </>
+    </section>
   );
 }
