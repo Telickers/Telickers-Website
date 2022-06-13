c4da35eef29bccaf8433859adf2766ecc761e1cb
Telickers <107256858+Telickers@users.noreply.github.com>
2022-06-13 13:02:43 +0300

    start into

--- components/Slider/Slide.jsx
+++ components/Slider/Slide.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Slide({ slide }) {
   return (
     <article className="relative flex h-[100vh] min-h-[400px] flex-col bg-[#D9D9D9] py-[100px] text-3xl">
-      <h2 className="mb-20 ml-20 text-left text-5xl font-bold underline underline-offset-8">
+      <h2 className="mb-20 ml-20 text-left text-5xl font-bold ">
         Customer Reviews
       </h2>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-1 lg:grid-cols-3">
         <div className="text-center">
           <img
             src={slide.img}
@@ -16,10 +16,10 @@ export default function Slide({ slide }) {
           <h3 className="font-bold">{slide.name}</h3>
           <h4 className="text-gray-500">{slide.comp}</h4>
         </div>
-        <div className="flex">
-          <p className="mt-20 text-[400px] font-light">{"{"}</p>
+        <div className="col-span-2 flex">
+          <p className="mt-20 text-[400px]">{"{"}</p>
           <h3 className="text-center text-5xl">{slide.text}</h3>
-          <p className="mt-20 text-[400px] font-light">{"}"}</p>
+          <p className="mt-20 text-[400px]">{"}"}</p>
         </div>
       </div>
       <div className="curve">
@@ -30,7 +30,7 @@ export default function Slide({ slide }) {
           preserveAspectRatio="none"
         >
           <path
-            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            d="M20285.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
             className="shape-fill"
           ></path>
         </svg>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,13 @@ import Services from "@/components/Services";
 import About from "@/components/About";
 import Work from "@/components/Work";
 import Reviews from "@/components/Slider/Reviews";
+import Intro from "@/components/Intro";
 
 export default function Home() {
   return (
     <>
       <div id="behind-the-animation">
+        <Intro />
         <About />
         <Services />
         <Work />
