6c7abd3dfb706cd760cd913c10b41b4f36912877
Telickers <107256858+Telickers@users.noreply.github.com>
2022-06-15 08:49:02 +0300

    about us

---
+++ components/AboutPage/Team.jsx
@@ -0,0 +1,17 @@
+export default function Team() {
+  return (
+    <div className="relative mb-20">
+      <img src="/team/lefthand.svg" alt="lefthand" className="w-1/4 " />
+      <img
+        src="/team/teamcloud.svg"
+        alt="teamcloud"
+        className=" mx-auto md:w-3/5"
+      />
+      <img
+        src="/team/righthand.svg"
+        alt="righthand"
+        className="ml-auto w-1/4 -translate-y-64"
+      />
+    </div>
+  );
+}
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,10 @@
+import Team from "@/components/AboutPage/Team";
+
 export default function About() {
   return (
     <>
-      <div
-        className="behind-the-animation flex h-screen w-screen items-center justify-center"
-      >
-        <h1 className="text-5xl font-bold text-black">About us</h1>
+      <div className="behind-the-animation relat flex h-screen w-screen items-center justify-center">
+        <Team />
       </div>
     </>
   );
