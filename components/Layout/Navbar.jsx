30c1f7bbd680b60307ffbe69d44c00d697bfb511
Telickers <hello@telickers.net>
2022-06-15 16:55:47 +0300

    Implement Mobile Sidebar

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   return (
@@ -36,15 +39,121 @@ function LargeNavbar() {
 }
 
 function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="flex py-6 pl-4 md:hidden">
-      <Link href="/">
-        <img
-          src="/telickers-logo.svg"
-          alt="Telickers Logo"
-          className="w-40 cursor-pointer"
-        />
-      </Link>
-    </nav>
+    <>
+      <nav className="flex items-center justify-between py-6 px-4 md:hidden">
+        {/* Logo */}
+        <Link href="/">
+          <img
+            src="/telickers-logo.svg"
+            alt="Telickers Logo"
+            className="w-40 cursor-pointer"
+          />
+        </Link>
+
+        {/* Toggle Button  */}
+        <ToggleMenuButton isOpen={isOpen} toggleOpen={toggleOpen} />
+      </nav>
+      {/* SideMenu */}
+      <SideMenu isOpen={isOpen} toggleOpen={toggleOpen} />
+    </>
+  );
+}
+
+function ToggleMenuButton({ isOpen, toggleOpen }) {
+  return (
+    <button className={`${isOpen && "open"}`} onClick={() => toggleOpen()}>
+      <svg className="hamburger" width={32} height={24}>
+        <line id="top" x1={0} y1={2} x2={32} y2={2} />
+        <line id="middle" x1={0} y1={12} x2={32} y2={12} />
+        <line id="bottom" x1={0} y1={22} x2={32} y2={22} />
+      </svg>
+      <span className="hidden">Toggle Nav</span>
+    </button>
+  );
+}
+
+function SideMenu({ isOpen, toggleOpen }) {
+  const router = useRouter();
+
+  const links = [
+    { name: "Home", to: "/", id: 1 },
+    { name: "About Us", to: "/about", id: 2 },
+    { name: "What We Do", to: "/services", id: 3 },
+    { name: "Our Work", to: "/work", id: 4 },
+    { name: "Contact Us", to: "/contact", id: 5 },
+  ];
+
+  const itemVariants = {
+    closed: {
+      opacity: 0,
+    },
+    open: { opacity: 1 },
+  };
+
+  const parentVariants = {
+    closed: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+      },
+    },
+  };
+
+  return (
+    <div
+      className="absolute right-0 top-[90px] z-50 h-[265px] md:hidden"
+      style={{ direction: "rtl" }}
+    >
+      <AnimatePresence>
+        {isOpen && (
+          <motion.aside
+            style={{ direction: "ltr" }}
+            className="h-full rounded-l-3xl bg-white"
+            initial={{ width: 0 }}
+            animate={{
+              width: 200,
+              transition: { duration: 0.5, type: "spring", stiffness: 180 },
+            }}
+            exit={{
+              width: 0,
+              transition: { delay: 0.55, duration: 0.5 },
+            }}
+          >
+            <motion.div
+              className="flex flex-col space-y-5 p-5 text-xl"
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={parentVariants}
+            >
+              {links.map(({ id, name, to }) => (
+                <motion.span
+                  key={id}
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setTimeout(() => router.push(to), 1000);
+                    toggleOpen();
+                  }}
+                  variants={itemVariants}
+                >
+                  {name}
+                </motion.span>
+              ))}
+            </motion.div>
+          </motion.aside>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,56 +1,13 @@
-import {
-  AnimatePresence,
-  domAnimation,
-  LazyMotion,
-  motion,
-} from "framer-motion";
-
 import Navbar from "@/components/Layout/Navbar";
 import Footer from "@/components/Layout/Footer";
 import "@/styles/globals.css";
 
-const animation = {
-  name: "Fade Back",
-  variants: {
-    initial: {
-      opacity: 0,
-      scale: 0.4,
-    },
-    animate: {
-      opacity: 1,
-      scale: 1,
-    },
-    exit: {
-      opacity: 0,
-      scale: 0.4,
-    },
-  },
-  transition: {
-    duration: 0.7,
-  },
-};
-
 function MyApp({ Component, pageProps, router }) {
-
   return (
     <>
       <Navbar />
-      {/* <LazyMotion features={domAnimation}>
-        <AnimatePresence exitBeforeEnter={true}> */}
-      <motion.div
-        key={router.route.concat(animation.name)}
-        className="page-wrap"
-        // initial="initial"
-        // animate="animate"
-        // exit="exit"
-        variants={animation.variants}
-        transition={animation.transition}
-      >
-        <Component {...pageProps} />
-        <Footer />
-      </motion.div>
-      {/* </AnimatePresence>
-      </LazyMotion> */}
+      <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
