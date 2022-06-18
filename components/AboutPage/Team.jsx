dcfb08cd89485d90a30fc952bbde8c2297f541f0
Telickers <hello@telickers.net>
2022-06-18 12:03:50 +0300

    Change Our team def section

--- components/AboutPage/Team.jsx
+++ components/AboutPage/Team.jsx
@@ -1,40 +1,20 @@
 export default function Team() {
   return (
-    <section className="flex h-screen w-screen items-center justify-center bg-[url('/abouticons/teamAll.svg')] bg-cover bg-center bg-no-repeat">
-      <article className="flex w-[50%] flex-col space-y-7 text-center text-white md:w-[25%]">
-        <span className="block text-4xl font-bold">Team Telickers</span>
-        <p className="text-lg">
-          in a nutshell, we are a group of developers and designers who found
-          that our skills are valuable. So we decided to merge our skills into
-          one team having a structure and the motivation for researching
-          prototyping and building technology. Tha is how we got to build our
-          team, and since then we have helped businesses freelancers, companies
-        </p>
-      </article>
+    <section className="flex flex-col items-center justify-center px-4 pb-14">
+      <h2 className="text-with-shadow my-28 text-center text-6xl font-bold leading-[4rem] tracking-wide">
+        We <br /> MAKE <br /> IMPACT
+      </h2>
+      <p className="mb-3 text-3xl font-bold">
+        <span className="text-2xl">Our Story...</span> Telickers Team
+      </p>
+      <p className="text-center text-lg">
+        In a nutshell, we are a group of developers and designers who found that
+        our skills are valuable. So we decided to merge our skills into one team
+        having a structure and the motivation for researching, prototyping and
+        building technology. That is how we got to build our team, and since
+        then we have helped businesses, freelancers {"&"} companies reach their
+        goals {"&"} improve their workflow.
+      </p>
     </section>
   );
 }
-
-/*  <img
-        src="/team/lefthand.svg"
-        alt="lefthand"
-        className="z-10 -mb-[150px] w-[250px] md:-mb-[350px] md:w-[600px]"
-      />
-
-      <div className="h-full w-full bg-[url('/team/cloudteam.svg')] bg-no-repeat">
-        <p className="">
-          <span className="mb-10 block text-4xl font-bold">Team Telickers</span>
-          in a nutshell, we are a group of developers and designers who found
-          that our skills are valuable. So we decided to merge our skills into
-          one team having a structure and the motivation for researching
-          prototyping and building technology. That&apos;s how we got to build
-          our team, and since then we have helped businesses freelancers,
-          companies
-        </p>
-      </div>
-
-      <img
-        src="/team/righthand.svg"
-        alt="righthand"
-        className="z-10 ml-auto -mt-[150px] w-[250px] md:-mt-[370px] md:w-[600px]"
-      /> */
--- components/AboutPage/Why.jsx
+++ components/AboutPage/Why.jsx
@@ -3,10 +3,12 @@ import RequestMeeting from "../Common/RequestMeeting";
 export default function Why() {
   return (
     <section className="flex items-center justify-center bg-brand py-12">
-      <div className="h-[400px] w-[330px] bg-[url('/abouticons/square.svg')] bg-contain bg-center bg-no-repeat md:w-[600px] md:h-[600px] md:bg-[url('/abouticons/circle.svg')]">
-        <div className="flex h-full flex-col items-center justify-center px-3 md:px-12 text-center">
-          <p className="mb-2 md:mb-6 text-3xl font-bold md:text-6xl">Why Telickers</p>
-          <p className="mb-4 md:mb-8 text-xl md:text-3xl">
+      <div className="h-[400px] w-[330px] bg-[url('/abouticons/square.svg')] bg-contain bg-center bg-no-repeat md:h-[600px] md:w-[600px] md:bg-[url('/abouticons/circle.svg')]">
+        <div className="flex h-full flex-col items-center justify-center px-3 text-center md:px-12">
+          <p className="mb-2 text-3xl font-bold md:mb-6 md:text-6xl">
+            Why Telickers
+          </p>
+          <p className="mb-4 text-xl md:mb-8 md:text-3xl">
             It all started when we focused on the sound of our keyboards,
             clicking for the duty of technology. Technology Clickers We are here
             for you!
