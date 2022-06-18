5f9537ae672ba3c6daec26dbc91e5a6be23014e4
Telickers <107256858+Telickers@users.noreply.github.com>
2022-06-18 22:08:11 +0300

    start with work page

---
+++ components/WorkPage/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section className="radial-bg flex h-screen flex-col items-center justify-center overflow-hidden text-center text-6xl">
+      <div className="text-linear mx-4 flex h-full items-center font-bold">
+        We work to sustain you, <br /> believing in your success
+      </div>
+    </section>
+  );
+}
--- pages/work.jsx
+++ pages/work.jsx
@@ -1,9 +1,9 @@
+import Hero from "@/components/WorkPage/Hero";
+
 export default function Work() {
   return (
     <>
-      <section className="flex h-screen flex-col items-center justify-center bg-[linear-gradient(38.54deg,#0F1A29_-58.14%,#080E17_32.07%,#0F1A29_106.6%)]">
-        <h1 className="text-5xl font-bold text-white">Work</h1>
-      </section>
+      <Hero />
     </>
   );
 }
