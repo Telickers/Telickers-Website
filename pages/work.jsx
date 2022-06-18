601aa75aa6690427feb82be1fee2bb069411ad36
Telickers <107256858+Telickers@users.noreply.github.com>
2022-06-18 23:32:18 +0300

    work page
    Hussein, do not comment on my code!!

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/WorkPage/Hero";
+import Projects from "@/components/WorkPage/Projects";
+import SearchBar from "@/components/WorkPage/SearchBar";
 
 export default function Work() {
   return (
     <>
       <Hero />
+      <SearchBar />
+      <Projects />
     </>
   );
 }
