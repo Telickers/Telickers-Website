37e48f6fa8d62a49f693fcfb28c78b71a2667a82
Telickers <hello@telickers.net>
2022-06-19 13:44:30 +0300

    Cleaning Work page

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,13 +1,9 @@
 import Hero from "@/components/WorkPage/Hero";
-import Projects from "@/components/WorkPage/Projects";
-import SearchBar from "@/components/WorkPage/SearchBar";
 
 export default function Work() {
   return (
     <>
       <Hero />
-      <SearchBar />
-      <Projects />
     </>
   );
 }
