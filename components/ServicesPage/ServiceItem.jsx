6e0932f4b2cf17ae73b4c37ebc5fa0317293cfa3
Telickers <hello@telickers.net>
2022-06-21 17:17:10 +0300

    Some fixes in the services page

--- components/ServicesPage/ServiceItem.jsx
+++ components/ServicesPage/ServiceItem.jsx
@@ -5,18 +5,19 @@ import { GoPrimitiveDot } from "react-icons/go";
 export default function ServiceItem({ service }) {
   return (
     <article className="flex w-screen flex-col space-y-3 px-5 lg:px-20">
+      {/* Service Title */}
       <div className="flex items-center">
         <div className="z-20 rounded-full bg-blue-300 p-3 text-blue-600">
           {service.Icon}
         </div>
-
-        <div className="z-10 -ml-4 flex w-[320px] items-center justify-center rounded-r-full bg-[#4415ff33] py-2 text-2xl font-bold text-[#190A55]">
+        <div className="z-10 -ml-4 flex w-[320px] items-center justify-center rounded-r-full bg-[#4415ff33] py-2 text-lg font-bold text-[#190A55] sm:text-xl md:text-2xl">
           <h2>{service.title}</h2>
         </div>
       </div>
 
-      <div className="grid grid-cols-1 gap-0 lg:grid-cols-2 lg:gap-3 ">
-        <div className="aspect-w-16 aspect-h-9 relative block overflow-hidden rounded-tr-md rounded-tl-md shadow-xl lg:hidden lg:rounded-tl-[0]">
+      <div className="grid grid-cols-1 gap-0 lg:grid-cols-2 lg:gap-3">
+        {/* Image for mobiles */}
+        <div className="aspect-w-16 aspect-h-9 relative block overflow-hidden rounded-tr-md rounded-tl-md lg:hidden lg:rounded-tl-[0]">
           <div className="absolute left-0 z-10 flex h-full w-full items-center justify-center bg-[#4315FF36] px-2 text-center text-3xl text-white">
             We Cover All Your <br /> Web Needs
           </div>
@@ -28,17 +29,19 @@ export default function ServiceItem({ service }) {
           />
         </div>
 
-        <div className="grid space-y-3 rounded-bl-md rounded-br-[50%] py-6 px-6 shadow-xl lg:grid-cols-2 lg:gap-3 lg:rounded-tl-md lg:rounded-br-[0%] lg:px-8">
+        {/* Content */}
+        <div className="grid space-y-3 rounded-bl-md rounded-br-[50%] py-6 px-6 shadow-xl lg:grid-cols-2 lg:gap-3 lg:space-y-0 lg:rounded-tl-md lg:rounded-br-[0%] lg:px-8">
           {service.details.map((detail, idx) => (
-            <p key={idx}>
+            <p key={idx} className="max-w-[80%]">
               <GoPrimitiveDot className="inline text-3xl text-gray-500" />
               {detail}
             </p>
           ))}
         </div>
 
+        {/* Image for large screens */}
         <div className="aspect-w-16 aspect-h-9 relative hidden overflow-hidden rounded-br-[50%] rounded-tr-md shadow-xl lg:block">
-          <div className="absolute left-0 z-10 flex h-full w-2/5 items-center justify-center bg-[#4315FF36] px-2 text-center text-5xl text-white">
+          <div className="absolute left-0 z-10 flex h-full w-2/5 items-center justify-center bg-[#4415ff57] px-2 text-center text-4xl text-white">
             We Cover All Your Web Needs
           </div>
           <Image
--- components/ServicesPage/Services.jsx
+++ components/ServicesPage/Services.jsx
@@ -1,12 +1,18 @@
-import { services } from "data";
+import RequestMeeting from "../Common/RequestMeeting";
 import ServiceItem from "./ServiceItem";
+import { services } from "data";
 
 export default function ServicesSection() {
   return (
-    <section className="flex flex-col items-center justify-center space-y-44 bg-brand py-20">
-      {services.map((service) => (
-        <ServiceItem service={service} key={service.title} />
-      ))}
+    <section className="bg-brand py-28">
+      <div className="flex flex-col items-center justify-center space-y-44">
+        {services.map((service) => (
+          <ServiceItem service={service} key={service.title} />
+        ))}
+      </div>
+      <div className="mt-28 flex justify-center">
+        <RequestMeeting />
+      </div>
     </section>
   );
 }
