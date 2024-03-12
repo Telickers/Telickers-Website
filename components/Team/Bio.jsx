c46ad8350061500ff8df6ec44b3d2fedff05f7a6
Hussein Hassan <hussein.h.tech@gmail.com>
2024-03-12 09:25:06 +0200

    Update Bio.jsx

--- components/Team/Bio.jsx
+++ components/Team/Bio.jsx
@@ -15,19 +15,6 @@ export default function Bio({ bio, facebookUrl, instagramUrl, linkedinUrl }) {
       className="mt-2 flex w-[350px] flex-col items-center rounded-lg p-4 text-center shadow-lg xs:w-[300px] xs:p-2"
     >
       <p className="py-1.5 font-medium text-slate-500">{bio}</p>
-      <div className="flex space-x-3 pt-5">
-        <a href={facebookUrl} target="_blank" rel="noreferrer">
-          <FaFacebookF />
-        </a>
-        <AiOutlineLine className="rotate-90 " />
-        <a href={instagramUrl} target="_blank" rel="noreferrer">
-          <FaInstagram />
-        </a>
-        <AiOutlineLine className="rotate-90 " />
-        <a href={linkedinUrl} target="_blank" rel="noreferrer">
-          <FaLinkedinIn />
-        </a>
-      </div>
     </motion.div>
   );
 }
